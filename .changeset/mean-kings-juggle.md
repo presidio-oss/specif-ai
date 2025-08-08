40467a4c96edb368fb77b8632d58fa9ada7d1f03
chidam <chidambaranathans@presidio.com>
2025-08-08 15:05:18 +0530

    "New" pill for new modules (#375)
    * new pill for new modules
    
    * case, font and color change
    
    * moved variable to appconstants
    
    * color changed once again
    
    ---------
    
    Co-authored-by: chidam333 <chidam.sync@gmail.com>

---
+++ .changeset/mean-kings-juggle.md
@@ -0,0 +1,5 @@
+---
+"specif-ai": patch
+---
+
+new tag pill
--- ui/src/app/components/sidebar/sidebar.component.ts
+++ ui/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { AsyncPipe, NgClass, NgForOf, NgIf } from '@angular/common';
 import { NgIconComponent } from '@ng-icons/core';
 import { BadgeComponent } from '../core/badge/badge.component';
@@ -9,6 +9,7 @@ import { FILTER_STRINGS } from '../../constants/app.constants';
 import { RequirementTypeEnum } from 'src/app/model/enum/requirement-type.enum';
 import { IconPairingEnum } from '../../model/enum/file-type.enum';
 import { ISelectedFolder } from 'src/app/model/interfaces/IList';
+import { APP_CONSTANTS } from '../../constants/app.constants';
 
 @Component({
   selector: 'app-sidebar',
@@ -24,12 +25,24 @@ import { ISelectedFolder } from 'src/app/model/interfaces/IList';
     NgForOf,
   ],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
   @Input() directories: { name: string; children: string[] }[] = [];
   @Input() selectedFolder: ISelectedFolder | null = null;
   @Input() appName: string = '';
   @Input() haiFolder: { key: string; value: string }[] = [];
   @Input() isCreatingSolution: boolean = false;
+  newModuleFlag: boolean = false;
+
+  ngOnInit(): void {
+    this.newModuleFlag = localStorage.getItem(APP_CONSTANTS.NEW_MODULE_FLAG) === 'true';
+    if (!this.newModuleFlag) {
+      localStorage.setItem(APP_CONSTANTS.NEW_MODULE_FLAG, 'false');
+    }
+  }
+
+  ngOnDestroy(): void {
+    localStorage.setItem(APP_CONSTANTS.NEW_MODULE_FLAG, 'true');
+  }
   
   @Output() folderSelected = new EventEmitter<any>();
   @Output() addDocument = new EventEmitter<string>();
